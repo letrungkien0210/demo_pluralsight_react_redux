6aa686e96c12af5c9f71bc4c460b2c2d5ba1f05c
ken <letrungkien0210@gmail.com>
2017-04-25 05:49:42 +0700

    Initial App Structure.

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, IndexRoute} from  'react-router';
+import App from './components/App';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+import CoursesPage from './components/course/CoursesPage';
+
+export default(
+  <Route path="/" components={App}>
+    <IndexRoute component={HomePage}/>
+    <Route path="courses" components={CoursesPage}/>
+    <Route path="about" component={AboutPage}/>
+  </Route>
+);
+
