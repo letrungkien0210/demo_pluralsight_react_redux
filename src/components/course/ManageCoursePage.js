f4261044ce4903991a246f5ae6fc23e80f43d59c
kien-ubuntu <letrungkien0210@gmail.com>
2017-05-04 21:09:20 +0700

    Create Form Submission Loading Indicator.

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -10,7 +10,8 @@ export class ManageCoursePage extends React.Component {
 
     this.state = {
       course: Object.assign({}, this.props.course),
-      errors: {}
+      errors: {},
+      saving: false
     }
 
     this.updateCourseState = this.updateCourseState.bind(this);
@@ -33,6 +34,7 @@ export class ManageCoursePage extends React.Component {
 
   saveCourse(event){
     event.preventDefault();
+    this.setState({saving: true});
     this.props.actions.saveCourse(this.state.course)
       .then(() => {
         this.redirect();
@@ -42,6 +44,7 @@ export class ManageCoursePage extends React.Component {
   }
 
   redirect(){
+    this.setState({saving: false});
     this.context.router.push('/courses');
   }
 
@@ -53,6 +56,7 @@ export class ManageCoursePage extends React.Component {
         onSave={this.saveCourse}
         course={this.state.course}
         errors={this.state.errors}
+        saving={this.state.saving}
       />
     );
   }
